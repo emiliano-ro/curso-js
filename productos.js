0bb746e9ab66d975bbafa562fbe7230bce66573a
Emiliano <emiliano.ro@hotmail.com>
2022-08-18 22:02:12 -0300

    Se está trabajando en el carrito

---
+++ productos.js
@@ -0,0 +1,63 @@
+let stock;
+let id;
+let nombre = "";
+let precio;
+
+const producto = [
+    {
+        nombre: "Turbo K03 Bora, Golf GTI, Audi A3 1.8T 180 HP",
+        id: 1,
+        precio: 65000,
+        stock: 5,
+        img: "../images/bturbo.jpg"
+    },
+    {
+        nombre: "Intercooler upgrade Vw Golf Mk7 Audi A3 S3 8v 2.0t 1.8t Ftx",
+        id: 2,
+        precio: 65000,
+        stock: 1,
+        img: "../images/intercoolermk5.jpg"
+    },
+    {
+        nombre: "Multiple de admision ORP VW Audi S3 GTI MK7 2.0 TSI MQB Gen3",
+        id: 3,
+        precio: 87000,
+        stock: 3,
+        img: "../images/admtsi.jpg"
+    },
+    {
+        nombre: "Multiple escape T3 Bipulsativo 2.0 tfsi tsi VW Audi - SPA",
+        id: 4,
+        precio: 42000,
+        stock: 0,
+        img: "../images/mavento.jpg"
+    },
+    {
+        nombre: "Cuerpos De Inyección 40mm Dcoe Fajs",
+        id: 5,
+        precio: 62000,
+        stock: 6,
+        img: "../images/cuerpos.jpg"
+    },
+    {
+        nombre: "Escape completo + downpipe acero inoxidable Toyota Yaris GR",
+        id: 6,
+        precio: 120000,
+        stock: 1,
+        img: "../images/downpipegr.jpg"
+    },
+    {
+        nombre: "Kit Agua Metanol AEM V3 1.15 Gall Bora Vento Golf Scirocco",
+        id: 7,
+        precio: 82000,
+        stock: 3,
+        img: "../images/metanol.jpg"
+    },
+    {
+        nombre: "Bielas forjadas VW Bora Vento Gol 144x22 800hp SAENZ",
+        id: 8,
+        precio: 250000,
+        stock: 5,
+        img: "../images/bielasvento.jpg"
+    },
+];
--- tienda.js
+++ tienda.js
@@ -1,66 +1,4 @@
-let stock;
-let id;
-let nombre = "";
-let precio;
-
-const producto = [
-    {
-        nombre: "Turbo K03 Bora, Golf GTI, Audi A3 1.8T 180 HP",
-        id: 1,
-        precio: 65000,
-        stock: 4,
-        img: "../images/bturbo.jpg"
-    },
-    {
-        nombre: "Intercooler upgrade Vw Golf Mk7 Audi A3 S3 8v 2.0t 1.8t Ftx",
-        id: 2,
-        precio: 65000,
-        stock: 1,
-        img: "../images/intercoolermk5.jpg"
-    },
-    {
-        nombre: "Multiple de admision ORP VW Audi S3 GTI MK7 2.0 TSI MQB Gen3",
-        id: 3,
-        precio: 87000,
-        stock: 3,
-        img: "../images/admtsi.jpg"
-    },
-    {
-        nombre: "Multiple escape T3 Bipulsativo 2.0 tfsi tsi VW Audi - SPA",
-        id: 4,
-        precio: 42000,
-        stock: 0,
-        img: "../images/mavento.jpg"
-    },
-    {
-        nombre: "Cuerpos De Inyección 40mm Dcoe Fajs",
-        id: 5,
-        precio: 62000,
-        stock: 6,
-        img: "../images/cuerpos.jpg"
-    },
-    {
-        nombre: "Escape completo + downpipe acero inoxidable Toyota Yaris GR",
-        id: 6,
-        precio: 120000,
-        stock: 1,
-        img: "../images/downpipegr.jpg"
-    },
-    {
-        nombre: "Kit Agua Metanol AEM V3 1.15 Gall Bora Vento Golf Scirocco",
-        id: 7,
-        precio: 82000,
-        stock: 3,
-        img: "../images/metanol.jpg"
-    },
-    {
-        nombre: "Bielas forjadas VW Bora Vento Gol 144x22 800hp SAENZ",
-        id: 8,
-        precio: 250000,
-        stock: 5,
-        img: "../images/bielasvento.jpg"
-    },
-];
+let carrito = [];
 
 let titProd = document.getElementById("titProd");
 titProd.className = "bTitulo eTitulo";
@@ -73,67 +11,85 @@ sectionProd.className = "row tienda-section";
 let main = document.getElementById("main-tienda");
 main.className = "dfondo";
 
-for (const prod of producto) {
+pintarProductos();
 
-    let sDesc = "";
-    let colorStock = "";
+function pintarProductos() {
 
-    if (prod.stock === 0) {
-        sDesc = "No disponible";
-        colorStock = "stockSin";
-    }
-    else if (prod.stock > 2) {
-        sDesc = "Disponible";
-        colorStock = "stockHay";
-    }
-    else if (prod.stock > 0 && prod.stock <= 2) {
-        sDesc = "Consultar";
-        colorStock = "stockCrit";
-    }
+    for (const prod of producto) {
 
-    let divCard = document.createElement("div");
-    divCard.className = "d-flex col-6 col-sm-6 col-xs-5 col-md-4 col-lg-3 col-xl-3 justify-content-center";
+        let sDesc = "";
+        let colorStock = "";
 
-    let divProd = document.createElement("div");
-    divProd.className = "anim producto aTiempo";
-    divProd.innerHTML = ` <img src=${prod.img} class="producto__imagen">
-    `;
+        if (prod.stock === 0) {
+            sDesc = "No disponible";
+            colorStock = "stockSin";
+        }
+        else if (prod.stock > 2) {
+            sDesc = "Disponible";
+            colorStock = "stockHay";
+        }
+        else if (prod.stock > 0 && prod.stock <= 2) {
+            sDesc = "Consultar";
+            colorStock = "stockCrit";
+        }
 
-    let divDesc = document.createElement("div");
+        let divCard = document.createElement("div");
+        divCard.className = "d-flex col-6 col-sm-6 col-xs-5 col-md-4 col-lg-3 col-xl-3 justify-content-center";
 
-    divDesc.className = "producto__desc";
-    divDesc.innerHTML = `
+        let divProd = document.createElement("div");
+        divProd.className = "anim producto aTiempo";
+        divProd.innerHTML = ` <img src=${prod.img} class="producto__imagen">
+        `;
+
+        let divDesc = document.createElement("div");
+
+        divDesc.className = "producto__desc";
+        divDesc.innerHTML = `
                     <h2 class="producto__titulo">${prod.nombre}</h2>
                     <h3 class="producto__precio">$${prod.precio}</h3>
                     <h4 class="producto__precio">Stock: <span class="${colorStock}"> ${sDesc}</span></h4>
                     <button class="producto__cbtn" id="${prod.id}" type="button"> Agregar al carrito </button>
-    `;
-    sectionProd.append(divCard);
-    divCard.append(divProd);
-    divProd.append(divDesc);
+        `;
 
-    let bodyT = document.getElementById("bodyTienda");
-    let divBtn = document.createElement("div");
-    divBtn.className = "divCarritoNo";
-    divBtn.innerHTML = `<p id="pBtn">Producto agregado al carrito!</p>
-    `;
+        sectionProd.append(divCard);
+        divCard.append(divProd);
+        divProd.append(divDesc);
 
-    let carBtn = document.getElementById(prod.id);
-
-    carBtn.onclick = () => {
-        carritoDiv();
-    }
-
-    bodyT.append(divBtn);
-
-    function carritoDiv() {
-        divBtn.className = "desdivprodAgregado";
-        console.log("Agregaste un prod");
-        setTimeout(carritoDivBorrar, 3000);
-    }
-
-    function carritoDivBorrar() {
+        let bodyT = document.getElementById("bodyTienda");
+        let divBtn = document.createElement("div");
         divBtn.className = "divCarritoNo";
-    }
+        divBtn.innerHTML = `<p id="pBtn">Producto agregado al carrito!</p>
+        `;
 
-};
+        let carBtn = document.getElementById(prod.id);
+
+        carBtn.onclick = () => {
+            carritoDiv();
+        }
+
+        bodyT.append(divBtn);
+
+        function carritoDiv() {
+            divBtn.className = "desdivprodAgregado";
+            console.log("Agregaste un prod");
+            setTimeout(carritoDivBorrar, 3000);
+        }
+
+        function carritoDivBorrar() {
+            divBtn.className = "divCarritoNo";
+        }
+
+    };
+
+    producto.forEach(prod => {
+        document.getElementById(`${prod.id}`).addEventListener("click", function(){
+            agregarAlcarrito(prod);
+        });
+    })
+}
+
+function agregarAlcarrito(productos) {
+    carrito.push(productos);
+    console.log(carrito);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
