835f6e4818b61d4a26912a3a42065413bd9e4bfd
Emiliano <emiliano.ro@hotmail.com>
2022-08-30 19:38:45 -0300

    Se optimizó la estructura

--- productos.js
+++ productos.js
@@ -3,6 +3,7 @@ let id;
 let nombre = "";
 let precio;
 let categoria = "";
+let destacado = "";
 
 const producto = [
     {
@@ -11,14 +12,16 @@ const producto = [
         precio: 65000,
         stock: 5,
         categoria: "turbos",
+        destacado: "no",
         img: "../images/bturbo.jpg"
     },
     {
         nombre: "Intercooler upgrade Vw Golf Mk7 Audi A3 S3 8v 2.0t 1.8t Ftx",
         id: 2,
         precio: 65000,
-        stock: 1,
+        stock: 3,
         categoria: "intercooler",
+        destacado: "si",
         img: "../images/intercoolermk5.jpg"
     },
     {
@@ -27,6 +30,7 @@ const producto = [
         precio: 87000,
         stock: 3,
         categoria: "multiplesadm",
+        destacado: "no",
         img: "../images/admtsi.jpg"
     },
     {
@@ -35,6 +39,7 @@ const producto = [
         precio: 42000,
         stock: 0,
         categoria: "multipleesc",
+        destacado: "no",
         img: "../images/mavento.jpg"
     },
     {
@@ -43,14 +48,16 @@ const producto = [
         precio: 62000,
         stock: 6,
         categoria: "cuerpos",
+        destacado: "no",
         img: "../images/cuerpos.jpg"
     },
     {
         nombre: "Escape completo + downpipe acero inoxidable Toyota Yaris GR",
         id: 6,
         precio: 120000,
-        stock: 1,
+        stock: 3,
         categoria: "escapes",
+        destacado: "si",
         img: "../images/downpipegr.jpg"
     },
     {
@@ -59,6 +66,7 @@ const producto = [
         precio: 82000,
         stock: 3,
         categoria: "metanol",
+        destacado: "no",
         img: "../images/metanol.jpg"
     },
     {
@@ -67,6 +75,70 @@ const producto = [
         precio: 250000,
         stock: 5,
         categoria: "bielas",
+        destacado: "si",
         img: "../images/bielasvento.jpg"
+    },
+    {
+        nombre: "Column Pod Vw Golf Bora Audi A3 Porta Relojes",
+        id: 9,
+        precio: 15000,
+        stock: 5,
+        categoria: "accesorios",
+        destacado: "si",
+        img: "../images/cpod.jpg"
+    },
+    {
+        nombre: "Kit admision directa REVO AUDI S4 S5 3.0 TFSI B8 / 8.5",
+        id: 10,
+        precio: 90000,
+        stock: 3,
+        categoria: "admision",
+        destacado: "si",
+        img: "../images/kadm.jpg"
+    },
+    {
+        nombre: "Bomba de nafta interna AEM 1.8T A3 Bora Golf 320lph 50-1000",
+        id: 11,
+        precio: 250000,
+        stock: 5,
+        categoria: "accesorios",
+        destacado: "si",
+        img: "../images/bcbora.jpg"
+    },
+    {
+        nombre: "Escape Downpipe VW Vento Scirocco Golf Seat 2.0TFSI TSI FTX",
+        id: 12,
+        precio: 80000,
+        stock: 3,
+        categoria: "escapes",
+        destacado: "si",
+        img: "../images/dpipev.jpg"
+    },
+    {
+        nombre: "Intercooler Universal 90cm X 30cm X 10cm Ftx Fueltech",
+        id: 13,
+        precio: 250000,
+        stock: 4,
+        categoria: "intercooler",
+        destacado: "si",
+        img: "../images/intercooler.jpg"
+    },
+    {
+        nombre: "Conjunto Central Turbo IS38 Audi S3 8v 2.0 TSI Master Power",
+        id: 14,
+        precio: 220000,
+        stock: 2,
+        categoria: "turbos",
+        destacado: "no",
+        img: "../images/nucleot.jpg"
+    },
+    {
+        nombre: "Bobinas red x4 VW Audi Scirocco Vento 2.0 TFSI TSI Bosch",
+        id: 15,
+        precio: 100000,
+        stock: 2,
+        categoria: "bobinas",
+        destacado: "no",
+        img: "../images/bobinasred.jpg"
     },
 ];
--- tienda.js
+++ tienda.js
@@ -2,7 +2,7 @@ let carrito = [];
 let totalCarrito = [];
 let tSumaCarrito;
 
-let cFiltros = ["accesorios", "combos", "repro", "turbos", "bielas"];
+let cFiltros = ["destacados", "combos", "repro", "turbos", "bielas"];
 let filtros = document.getElementById("barrafil");
 filtros.innerHTML = `<nav id="nav" class="tNav nuLi efecto aTiempo">
                     <ul class="aCajas liFondo eTexto dMenu sTexto dLi tsLi">
@@ -35,71 +35,15 @@ sectionProd.className = "row tienda-section";
 let main = document.getElementById("main-tienda");
 main.className = "dfondo";
 
-pintarProductos();
-
 const turbo = producto.filter((el) => el.categoria.includes("turbos"));
 const biela = producto.filter((el) => el.categoria.includes("bielas"));
-
-function pintarProductos() {
-
-    prodh1.innerText="Productos destacados";
-    sectionProd.innerHTML = ``;
-
-    for (const prod of producto) {
-
-        let sDesc = "";
-        let colorStock = "";
-
-        if (prod.stock === 0) {
-            sDesc = "No disponible";
-            colorStock = "stockSin";
-        }
-        else if (prod.stock > 2) {
-            sDesc = "Disponible";
-            colorStock = "stockHay";
-        }
-        else if (prod.stock > 0 && prod.stock <= 2) {
-            sDesc = "Consultar";
-            colorStock = "stockCrit";
-        }
-
-        let divCard = document.createElement("div");
-        divCard.className = "d-flex col-6 col-sm-6 col-xs-5 col-md-4 col-lg-3 col-xl-3 justify-content-center";
-
-        let divProd = document.createElement("div");
-        divProd.className = "anim producto aTiempo";
-        divProd.innerHTML = ` <img src=${prod.img} class="producto__imagen">
-        `;
-
-        let divDesc = document.createElement("div");
-
-        divDesc.className = "producto__desc";
-        divDesc.innerHTML = `
-                    <h2 class="producto__titulo">${prod.nombre}</h2>
-                    <h3 class="producto__precio">$${prod.precio}</h3>
-                    <h4 class="producto__precio">Stock: <span class="${colorStock}"> ${sDesc}</span></h4>
-                    <button class="producto__cbtn" id="${prod.id}" type="button"> Agregar al carrito </button>
-        `;
-
-        sectionProd.append(divCard);
-        divCard.append(divProd);
-        divProd.append(divDesc);
-
-    };
-
-    producto.forEach(prod => {
-        document.getElementById(`${prod.id}`).addEventListener("click", function () {
-            agregarAlcarrito(prod);
-            totalCarrito.push(prod.precio);
-            tSumaCarrito = totalSumado(...totalCarrito);
-            console.log(tSumaCarrito);
-        });
-    });
-}
+const destacados = producto.filter((el) => el.destacado.includes("si"));
 
 const totalSumado = (...numeros) => {
     return numeros.reduce((acc, numero) => acc + numero, 0);
 };
+
+pintar(destacados);
 
 function agregarAlcarrito(productos) {
     carrito.push(productos);
@@ -121,23 +65,23 @@ cFiltros.forEach(filtros => {
     document.getElementById(`${filtros}`).addEventListener("click", function () {
 
         if (filtros == "turbos") {
-            pintarTurbos();
+            pintar(turbo);
         }
         else if (filtros == "bielas") {
-            pintarBielas();
+            pintar(biela);
         }
-        else if(filtros == "accesorios"){
-            pintarProductos();
+        else if (filtros == "destacados") {
+            pintar(destacados);
         }
+
     });
 });
 
-function pintarTurbos() {
+function pintar(el) {
 
-    prodh1.innerText = "Turbos";
     sectionProd.innerHTML = ``;
 
-    for (const prod of turbo) {
+    for (const prod of el) {
 
         let sDesc = "";
         let colorStock = "";
@@ -176,71 +120,14 @@ function pintarTurbos() {
         sectionProd.append(divCard);
         divCard.append(divProd);
         divProd.append(divDesc);
-
-        turbo.forEach(prod => {
-            document.getElementById(`${prod.id}`).addEventListener("click", function () {
-                agregarAlcarrito(prod);
-                totalCarrito.push(prod.precio);
-                tSumaCarrito = totalSumado(...totalCarrito);
-                console.log(tSumaCarrito);
-            });
-        });
     };
-}
-
-function pintarBielas(){
     
-    prodh1.innerText = "Bielas";
-    sectionProd.innerHTML = ``;
-
-    for (const prod of biela) {
-
-        let sDesc = "";
-        let colorStock = "";
-
-        if (prod.stock === 0) {
-            sDesc = "No disponible";
-            colorStock = "stockSin";
-        }
-        else if (prod.stock > 2) {
-            sDesc = "Disponible";
-            colorStock = "stockHay";
-        }
-        else if (prod.stock > 0 && prod.stock <= 2) {
-            sDesc = "Consultar";
-            colorStock = "stockCrit";
-        }
-
-        let divCard = document.createElement("div");
-        divCard.className = "d-flex col-6 col-sm-6 col-xs-5 col-md-4 col-lg-3 col-xl-3 justify-content-center";
-
-        let divProd = document.createElement("div");
-        divProd.className = "anim producto aTiempo";
-        divProd.innerHTML = ` <img src=${prod.img} class="producto__imagen">
-        `;
-
-        let divDesc = document.createElement("div");
-
-        divDesc.className = "producto__desc";
-        divDesc.innerHTML = `
-                    <h2 class="producto__titulo">${prod.nombre}</h2>
-                    <h3 class="producto__precio">$${prod.precio}</h3>
-                    <h4 class="producto__precio">Stock: <span class="${colorStock}"> ${sDesc}</span></h4>
-                    <button class="producto__cbtn" id="${prod.id}" type="button"> Agregar al carrito </button>
-        `;
-
-        sectionProd.append(divCard);
-        divCard.append(divProd);
-        divProd.append(divDesc);
-
-        biela.forEach(prod => {
-            document.getElementById(`${prod.id}`).addEventListener("click", function () {
-                agregarAlcarrito(prod);
-                totalCarrito.push(prod.precio);
-                tSumaCarrito = totalSumado(...totalCarrito);
-                console.log(tSumaCarrito);
-            });
+    el.forEach(prod => {
+        document.getElementById(`${prod.id}`).addEventListener("click", function () {
+            agregarAlcarrito(prod);
+            totalCarrito.push(prod.precio);
+            tSumaCarrito = totalSumado(...totalCarrito);
+            console.log(tSumaCarrito);
         });
-
-    };
+    });
 }
